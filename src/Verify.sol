ce5932f5c7cf275c3fbf92235d09c48db30d3ee9
freeman <xiongchenyu6@gmail.com>
2022-12-07 05:24:44 +0800

    Feature: init commit

---
+++ src/Verify.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Verify {
+
+    solve {
+        question address;
+        anwser address;
+    }
+
+    deposit();
+    withdraw();
+        verify(solve); # vrf random check
+    
+}
